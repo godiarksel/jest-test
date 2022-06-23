c07c19986ce29780d365106db6320ab54e121439
godfrey <godiarksel@gmail.com>
2022-06-23 18:46:32 +0300

    Run all exercise tests

--- js/string.test.js
+++ js/string.test.js
@@ -1,5 +1,7 @@
 const stringLength = require('./stringLength');
 const reverseString = require('./reverseString');
+const capitalize = require('./capitalize');
+const Calc = require('./calculator');
 // Task 1
 test('check string length accuracy', () =>{
   expect(stringLength('books')).toBe(5);
@@ -12,4 +14,24 @@ test('check string length requirements',() => {
 // Task 2
 test('check if string is properly reversed',() => {
   expect(reverseString('rabbit')).toBe('tibbar');
+});
+// Task 3
+describe('Calculator Tests', () => {
+  const testCase = new Calc(8, 4);
+  test('add numbers',() => {
+    expect(testCase.add()).toEqual(12);
+  });
+  test('subtract numbers',() => {
+    expect(testCase.subtract()).toEqual(4);
+  });
+  test('divide numbers',() => {
+    expect(testCase.divide()).toEqual(2);
+  });
+  test('multiply numbers',() => {
+    expect(testCase.multiply()).toEqual(32);
+  });
+});
+// Task 4
+test('check if the first character is capitalized',() => {
+  expect(capitalize('happy')).toBe('Happy');
 });
